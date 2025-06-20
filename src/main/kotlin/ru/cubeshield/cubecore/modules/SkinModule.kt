b52f7db90bb61ea0eff0fcaa538b759a00d3d2cc
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2025-06-20 13:31:59 +0400

    feat(modules): skin

--- src/main/kotlin/ru/cubeshield/cubecore/modules/SkinModule.kt
+++ src/main/kotlin/ru/cubeshield/cubecore/modules/SkinModule.kt
@@ -9,19 +9,6 @@ import ru.cubeshield.cubecore.CubeCore
 import ru.cubeshield.cubecore.api.ApiClient
 import ru.cubeshield.cubecore.config.ModConfig
 import ru.cubeshield.cubecore.event.*
-import java.util.concurrent.ConcurrentHashMap
-
-import com.mojang.brigadier.arguments.StringArgumentType
-import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
-import net.minecraft.server.command.CommandManager.*
-import net.minecraft.text.Text
-import net.minecraft.util.Identifier
-import net.minecraft.util.Util
-import java.util.*
-import net.minecraft.util.Pair
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket.Action
 
 
 class SkinModule : ICubeModule {
@@ -29,26 +16,26 @@ class SkinModule : ICubeModule {
     override val name = "Skin Module"
     override val description = "Модуль, отвечающий за скины игроков"
 
-    private var cachedPlayersSkins = ConcurrentHashMap<String, String>()
 
     override fun initialize(eventBus: EventBus, apiClient: ApiClient, config: ModConfig, modScope: CoroutineScope) {
-
         eventBus.subscribe<PlayerAuthorized> { (player, _, apiPlayer, _) ->
-            logger.info(apiPlayer.skin)
-            val textureJson = """
-            {
-              "textures": {
-                "SKIN": {
-                  "url": "http://textures.minecraft.net/texture/9484ef5035334ea57980d9d85e1660c74a074f080a24f01d2d26e520ddbd5eb4"
-                }
-              }
-            }
-            """
-            val value = Base64.getEncoder().encodeToString(textureJson.toByteArray())
-            val profile = player.gameProfile
-            profile.properties.removeAll("textures")
-            profile.properties.put("textures", Property("textures", value))
 
+
+            val playername = player.gameProfile.name
+            val skinType = if (apiPlayer.isSlim) {"slim"} else {"classic"}
+            val commandString = "skin set web $skinType ${apiPlayer.skin} $playername"
+
+            val server = player.server ?: return@subscribe
+            server.execute {
+                try {
+                    val source = server.commandSource
+                    server.commandManager.dispatcher.execute(commandString, source)
+
+                    logger.info("Skin for $playername (${apiPlayer.id}) has handled")
+                } catch (e: Exception) {
+                    CubeCore.LOGGER.error("Error while handling skin for $playername (${apiPlayer.id})", e)
+                }
+            }
         }
 
     }
