9a28f48ede81c1f345b6cd41f0c3960d9b2980e2
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2025-07-06 04:00:59 +0400

    feat(modules): cocktails

--- src/main/kotlin/ru/cubeshield/cubecore/modules/CocktailModule.kt
+++ src/main/kotlin/ru/cubeshield/cubecore/modules/CocktailModule.kt
@@ -1,13 +1,26 @@
 package ru.cubeshield.cubecore.modules
 
 
+import com.mojang.serialization.Codec
+import com.mojang.serialization.DataResult
+import com.mojang.serialization.MapDecoder
 import kotlinx.coroutines.*
+import net.fabricmc.fabric.api.event.player.UseItemCallback
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder
+import net.minecraft.component.ComponentType
+import net.minecraft.component.DataComponentTypes
+import net.minecraft.component.type.NbtComponent
 import net.minecraft.item.Items
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtOps
+import net.minecraft.text.Text
+import net.minecraft.util.ActionResult
 import ru.cubeshield.cubecore.api.ApiClient
 import ru.cubeshield.cubecore.config.ModConfig
 import ru.cubeshield.cubecore.event.*
+import ru.cubeshield.cubecore.potion.CustomPotion
 import ru.cubeshield.cubecore.potion.CustomPotionFactory
+import kotlin.math.log
 
 
 class CocktailModule : ICubeModule {
@@ -17,11 +30,26 @@ class CocktailModule : ICubeModule {
 
 
     override fun initialize(eventBus: EventBus, apiClient: ApiClient, config: ModConfig, modScope: CoroutineScope) {
-        FabricBrewingRecipeRegistryBuilder.BuildCallback { builder ->
-            builder.registerItemRecipe(Items.DIAMOND, Items.IRON_INGOT, CustomPotionFactory.createPotion().item)
+        eventBus.subscribe<PlayerAuthorized> { (player) ->
+            CustomPotion.entries.map { potion ->
+                player.giveItemStack(CustomPotionFactory.createPotion(potion))
+            }
         }
-    }
 
+        UseItemCallback.EVENT.register(UseItemCallback { player, world, hand ->
+            val stack = player.getStackInHand(hand)
+
+            if (world.isClient) return@UseItemCallback ActionResult.PASS
+
+            if (stack.item == Items.POTION) {
+                val nbtComponent = stack.get(DataComponentTypes.CUSTOM_DATA)
+                val id = nbtComponent?.copyNbt()?.get("custom_potion_id") ?: return@UseItemCallback ActionResult.PASS
+                val potion = CustomPotion.valueOf(id.toString().trim('"'))
+            }
+
+            return@UseItemCallback ActionResult.PASS
+        })
+    }
 
     override fun shutdown() {}
 }
--- src/main/kotlin/ru/cubeshield/cubecore/potion/CustomPotionFactory.kt
+++ src/main/kotlin/ru/cubeshield/cubecore/potion/CustomPotionFactory.kt
@@ -1,33 +1,27 @@
 package ru.cubeshield.cubecore.potion
 
-import net.minecraft.component.Component
-import net.minecraft.component.ComponentType
 import net.minecraft.component.DataComponentTypes
 import net.minecraft.component.type.LoreComponent
+import net.minecraft.component.type.NbtComponent
 import net.minecraft.component.type.PotionContentsComponent
-import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
-import net.minecraft.potion.Potions
-import net.minecraft.registry.entry.RegistryEntry
-import net.minecraft.text.Text
+import net.minecraft.nbt.NbtCompound
 import java.util.*
 
 object CustomPotionFactory {
-    fun createPotion(): ItemStack {
+    fun createPotion(customPotion: CustomPotion): ItemStack {
         val potionStack = ItemStack(Items.POTION)
-        potionStack.set(DataComponentTypes.CUSTOM_NAME,
-            Text.literal("Окак"))
-
-        potionStack.set(DataComponentTypes.LORE, LoreComponent(listOf(
-            Text.literal("ss"),
-            Text.literal("dasd")
-        )))
-
+        potionStack.set(DataComponentTypes.CUSTOM_NAME, customPotion.displayName)
+        if (customPotion.description != null) {
+            potionStack.set(DataComponentTypes.LORE, LoreComponent(listOf(customPotion.description)))
+        }
         potionStack.set(DataComponentTypes.POTION_CONTENTS,
-            PotionContentsComponent(Optional.of(Potions.STRENGTH), Optional.of(2) , mutableListOf(), Optional.of("s"))
+            PotionContentsComponent(Optional.empty(), Optional.of(customPotion.color), customPotion.effects, Optional.empty()),
         )
-
+        val nbt = NbtCompound()
+        nbt.putString("custom_potion_id", customPotion.name)
+        potionStack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbt))
         return potionStack;
     }
 }
