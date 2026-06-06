941898bf6b662709e13d756dbf0183d3f0881769
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2026-06-06 11:18:03 +0300

    fix

--- src/main/kotlin/ru/cubeshield/cubecore/api/dto/SessionCreateDto.kt
+++ src/main/kotlin/ru/cubeshield/cubecore/api/dto/SessionCreateDto.kt
@@ -1,15 +1,13 @@
 package ru.cubeshield.cubecore.api.dto
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ru.cubeshield.cubecore.domain.SessionEntity
 
 @Serializable
 data class SessionCreateDto (
-    @SerialName("login_time") val loginTime: Instant,
-    @SerialName("logout_time") val logoutTime: Instant,
+    @SerialName("login_time") val loginTime: kotlin.time.Instant,
+    @SerialName("logout_time") val logoutTime: kotlin.time.Instant,
     @SerialName("duration_seconds") val durationSeconds: Int,
     @SerialName("total_afk_seconds") val totalAfkSeconds: Int,
     @SerialName("total_active_seconds") val totalActiveSeconds: Int,
@@ -17,7 +15,7 @@ data class SessionCreateDto (
 ) {
     companion object {
         fun fromEntity (sessionEntity: SessionEntity): SessionCreateDto {
-            val logoutTime = Clock.System.now()
+            val logoutTime = kotlin.time.Clock.System.now()
             val durationSeconds = (logoutTime - sessionEntity.loginTime).inWholeSeconds.toInt()
             val totalAfkSeconds = sessionEntity.afkSeconds
             val totalActiveSeconds = durationSeconds - totalAfkSeconds
