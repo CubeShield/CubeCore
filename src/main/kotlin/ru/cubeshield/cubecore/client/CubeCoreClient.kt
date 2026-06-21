ac0e9d6004842c4fdf2a26911c00e7d0321590c0
Egor Fadeev <fad.egor05@gmail.com>
2026-06-21 13:05:17 +0300

    fixed

---
+++ src/main/kotlin/ru/cubeshield/cubecore/client/CubeCoreClient.kt
@@ -0,0 +1,15 @@
+package ru.cubeshield.cubecore.client
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+
+@Environment(EnvType.CLIENT)
+class CubeCoreClient : ClientModInitializer {
+
+
+
+    override fun onInitializeClient() {
+
+    }
+}
